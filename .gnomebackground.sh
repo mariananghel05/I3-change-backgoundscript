a97250999e5aff0ba6f268e627070e4a57f93052
mariananghel05 <mariananghel99@gmail.com>
2020-06-02 02:53:46 +0300

    Update .gnomebackground.sh

--- .gnomebackground.sh
+++ .gnomebackground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dinput () {
-(ls /home/marian05/wallpapers | dmenu -i -p "Background Image:") <&-
+(ls $HOME/wallpapers | dmenu -i -p "Background Image:") <&-
 }
 url=$(dinput)
-gsettings set org.gnome.desktop.background picture-uri file:/home/marian05/wallpapers/$url
+gsettings set org.gnome.desktop.background picture-uri file:$HOME/wallpapers/$url
