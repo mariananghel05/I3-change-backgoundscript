a4fc25f0e6e19e62659f89eb2e7f1770a7f7804b
mariananghel05 <mariananghel99@gmail.com>
2020-06-02 03:44:57 +0300

    Delete .changevideobackground.sh

--- .changevideobackground.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-dinput () {
-(ls $HOME/Wallpapers/VideoWallpapers | dmenu -p "Background Video:") <&-
-}
-url=$(dinput)
-pkill -f mplayer
-mplayer -loop 0 -rootwin -ao null -noconsolecontrols -fs $HOME/Wallpapers/VideoWallpapers/$url
-
