9a070f8e538c7dfe3c1ca10bbd8d0cd5c7f3ebc7
mariananghel05 <mariananghel99@gmail.com>
2020-06-16 23:58:48 +0300

    Delete .gnomebackground.sh

--- .gnomebackground.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dinput () {
-(ls $HOME/wallpapers | dmenu -i -p "Background Image:") <&-
-}
-url=$(dinput)
-gsettings set org.gnome.desktop.background picture-uri file:$HOME/wallpapers/$url
