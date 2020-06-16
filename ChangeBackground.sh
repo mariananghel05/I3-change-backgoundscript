a7ad81634a1371d72e6e6adef96e978abd904769
mariananghel05 <mariananghel99@gmail.com>
2020-06-17 02:27:54 +0300

    Update ChangeBackground.sh

--- ChangeBackground.sh
+++ ChangeBackground.sh
@@ -1,12 +1,16 @@
 #!bind/bash 
 
 
-#Dmenu Select Wallpaper  
+#Dmenu Select Wallpaper 
 dinput () {
-	    (ls /home/marian05/Wallpapers -I "VideoWallpapers"   | dmenu -i -p "Background Image:") <&-
+(ls $HOME/Wallpapers/ | dmenu -i -p "Select Section") <&-
+} 
+		url=$(dinput)
+dinput () {
+	    (ls /home/marian05/Wallpapers/$url   | dmenu -i -p "Background Image:") <&-
 	    }
 #Set The Wallpaper
-	    url=$(dinput)
-        feh --bg-scale /home/marian05/Wallpapers/$url
+	    url2="$url/$(dinput)"
+        feh --bg-scale /home/marian05/Wallpapers/$url2
 #Save name of Wallpaper for Startup
-		rm $HOME/MyScripts/Background.txt && touch $HOME/MyScripts/Background.txt && echo "$url" >> $HOME/MyScripts/Background.txt
+		rm $HOME/MyScripts/Background.txt && touch $HOME/MyScripts/Background.txt && echo "$url2" >> $HOME/MyScripts/Background.txt
